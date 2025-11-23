28134104601431aa375a297149fb72bf76cf2b0a
Sebastian Böhm <sboe0705@icloud.com>
2025-11-23 09:18:23 +0100

    added health check to Keycloak container

--- keycloak/keycloak-start.sh
+++ keycloak/keycloak-start.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
 docker compose up -d
+
+interval=10
+timeout=5
+retries=5
+start_period=10
+
+echo "[INFO]  Waiting for Keycloak to start ..."
+sleep $start_period
+iteration=0
+while [ $iteration -lt $((retries)) ]; do
+  curl --head -f --max-time $timeout http://localhost:9000/health/ready > /dev/null 2>&1 && echo "[INFO]  Keycloak started successfully!" && exit 0
+  iteration=$((iteration + 1))
+  sleep $interval
+done
+echo "[ERROR] Keycloak start failed! Use \"docker compose up\" to see the logs."
+exit 1

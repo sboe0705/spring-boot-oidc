5bd094f6b16f4fe0edaae61e32da933121418094
Sebastian Böhm <sboe0705@icloud.com>
2025-11-27 06:33:36 +0100

    added logout support

--- frontend/src/main/java/io/github/sboe0705/sample/oidc/frontend/SecurityConfiguration.java
+++ frontend/src/main/java/io/github/sboe0705/sample/oidc/frontend/SecurityConfiguration.java
@@ -1,13 +1,20 @@
 package io.github.sboe0705.sample.oidc.frontend;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 @Configuration
 public class SecurityConfiguration {
+
+    @Autowired
+    private ClientRegistrationRepository clientRegistrationRepository;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -17,9 +24,20 @@ public class SecurityConfiguration {
                 )
                 .oauth2Login(Customizer.withDefaults())
                 .oauth2Client(Customizer.withDefaults())
+                .logout(logout -> logout
+                        .logoutSuccessHandler(oidcLogoutSuccessHandler())
+                )
                 .csrf(csrf -> csrf.disable())
                 .cors(cors -> cors.disable());
         return http.build();
+    }
+
+    @Bean
+    LogoutSuccessHandler oidcLogoutSuccessHandler() {
+        OidcClientInitiatedLogoutSuccessHandler handler =
+                new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
+        handler.setPostLogoutRedirectUri("{baseUrl}");
+        return handler;
     }
 
 }

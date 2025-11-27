42cc22e3b8c087340eb0c05459451cce34e50f35
Sebastian Böhm <sboe0705@icloud.com>
2025-11-27 21:37:46 +0100

    logging out user info and token

--- frontend/src/main/java/io/github/sboe0705/sample/oidc/frontend/GuiController.java
+++ frontend/src/main/java/io/github/sboe0705/sample/oidc/frontend/GuiController.java
@@ -1,6 +1,10 @@
 package io.github.sboe0705.sample.oidc.frontend;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,11 +12,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class GuiController {
 
+    private static final Logger logger = LoggerFactory.getLogger(GuiController.class);
+
     @Autowired
     private UsersClient usersClient;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @AuthenticationPrincipal OidcUser user) {
+        logger.info("Logged in user: {} ({})", user.getFullName(), user.getPreferredUsername());
+        logger.info("Used JWT: {}", user.getIdToken().getTokenValue());
         UserInfo userInfo = usersClient.getUserInfo();
         model.addAttribute("user", userInfo);
         return "index";

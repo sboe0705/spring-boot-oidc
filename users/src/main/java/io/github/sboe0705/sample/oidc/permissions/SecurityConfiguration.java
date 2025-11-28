9d5c1114bd4ddcb6b125ac76c275a807c46bded8
Sebastian Böhm <sboe0705@icloud.com>
2025-11-28 05:58:40 +0100

    implemented resource server

---
+++ users/src/main/java/io/github/sboe0705/sample/oidc/permissions/SecurityConfiguration.java
@@ -0,0 +1,32 @@
+package io.github.sboe0705.sample.oidc.permissions;
+
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(Customizer.withDefaults())
+                );
+        return http.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder(OAuth2ResourceServerProperties properties) {
+        return JwtDecoders.fromIssuerLocation(properties.getJwt().getIssuerUri());
+    }
+
+}
--- users/src/main/java/io/github/sboe0705/sample/oidc/permissions/UsersController.java
+++ users/src/main/java/io/github/sboe0705/sample/oidc/permissions/UsersController.java
@@ -1,9 +1,12 @@
 package io.github.sboe0705.sample.oidc.permissions;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -13,9 +16,10 @@ public class UsersController {
     private PermissionsClient permissionsClient;
 
     @GetMapping("/user")
-    public UserInfo getUserInfo(/*Principal principal*/) {
+    public UserInfo getUserInfo(@AuthenticationPrincipal Jwt jwt) {
+        String name = jwt.getClaimAsString("name");
         List<String> permissions = permissionsClient.getPermissions();
-        return new UserInfo("name", "role", permissions);
+        return new UserInfo(name, "role", permissions);
     }
 
 }

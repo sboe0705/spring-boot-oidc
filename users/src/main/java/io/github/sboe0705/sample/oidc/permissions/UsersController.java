01aae21b51672694d290b07754c540b172411d35
Sebastian Böhm <sboe0705@icloud.com>
2025-11-28 06:06:23 +0100

    returning real name in users service

--- users/src/main/java/io/github/sboe0705/sample/oidc/permissions/UsersController.java
+++ users/src/main/java/io/github/sboe0705/sample/oidc/permissions/UsersController.java
@@ -6,7 +6,6 @@ import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.security.Principal;
 import java.util.List;
 
 @RestController
